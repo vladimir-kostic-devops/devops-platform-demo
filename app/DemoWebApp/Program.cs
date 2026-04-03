1d64753dca4aee86117d25bd1c0625e0f02435a5
vladimirkostic-kole <Vladimir.kostic@foreach.rs>
2026-04-03 14:58:42 -0400

    chore: remove duplicate README.MD, bump app version to trigger CI

--- app/DemoWebApp/Program.cs
+++ app/DemoWebApp/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Demo Web App - Running");
+app.MapGet("/", () => "Demo Web App v1.1 - Running");
 app.MapGet("/health/live", () => Results.Ok(new { status = "Healthy" }));
 app.MapGet("/health/ready", () => Results.Ok(new { status = "Ready" }));
 
